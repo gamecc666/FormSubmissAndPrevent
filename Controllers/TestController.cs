da3e505f3c4cce1d127873642b90b0542f94e95b
gamecc666 <995611928@qq.com>
2019-07-02 00:50:54 +0800

    project init

---
+++ Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FormSubmissAndPrevent.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Index(string name,string gender)
+        {         
+            return Content(name + " || " + gender);
+        }
+    }
+}
